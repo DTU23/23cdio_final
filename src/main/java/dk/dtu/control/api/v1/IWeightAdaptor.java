1ab45ccf46d4377cf23a8abe9149fac6c986297c
Frederik Værnegaard <frederik@nyka.dk>
2017-06-05 12:28:24 +0200

    moved AdaptorException to its own class

--- src/main/java/dk/dtu/control/api/v1/IWeightAdaptor.java
+++ src/main/java/dk/dtu/control/api/v1/IWeightAdaptor.java
@@ -2,12 +2,6 @@ package dk.dtu.control.api.v1;
 
 public interface IWeightAdaptor {
 	
-	class AdaptorException extends Exception {
-		private static final long serialVersionUID = -7306466308871895237L;
-		public AdaptorException(String msg) { super(msg); }
-		public AdaptorException(Exception e) { super(e); }		
-	}
-	
 	public void establishConnection(String ip, int port) throws AdaptorException;
 	public void setupUnit() throws AdaptorException;
 	

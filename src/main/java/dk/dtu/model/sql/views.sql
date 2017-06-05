7b7e1c9e0731bf5933711edba0e77f175b88a03f
Unknown <spirem@gmail.com>
2017-06-05 15:20:00 +0200

    db hot fix

--- src/main/java/dk/dtu/model/sql/views.sql
+++ src/main/java/dk/dtu/model/sql/views.sql
@@ -47,7 +47,7 @@ product_batch_component (Pharmacist)
 Task 9 - Creates a view to display various information across recipe, productbatch, productbatchcomponent and producebatch + produce
 */
 CREATE OR REPLACE VIEW product_batch_component_overview AS
-  SELECT pb_id, recipe.recipe_id, recipe_name, productbatch.status, produce_name, netto, opr_id
+  SELECT pb_id, rb_id, recipe.recipe_id, recipe_name, productbatch.status, produce_name, netto, opr_id
   FROM recipe NATURAL JOIN productbatch NATURAL JOIN productbatchcomponent
   NATURAL JOIN producebatch NATURAL JOIN produce
   ORDER BY pb_id;

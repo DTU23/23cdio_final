50505d0b352ecf480557107f8b95d57ec0bd22a0
Frederik Værnegaard <frederik@nyka.dk>
2017-06-05 15:23:58 +0200

    added main

---
+++ src/main/java/dk/dtu/MainWeighProcess.java
@@ -0,0 +1,15 @@
+package dk.dtu;
+
+import dk.dtu.control.IWeightProcessController;
+import dk.dtu.control.WeightProcessController;
+
+public class MainWeighProcess {
+
+	public static void main(String[] args) {
+
+		IWeightProcessController weightProcessController = new WeightProcessController("localhost", 8000);
+		weightProcessController.run();
+		
+	}
+
+}

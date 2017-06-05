81090059a80ca9b21ea0fa19719fdaaca221c143
Christian Niemann <spirem@gmail.com>
2017-06-05 17:43:56 +0200

    ValidationTest

--- src/test/java/dk/dtu/model/ValidationTest.java
+++ src/test/java/dk/dtu/model/ValidationTest.java
@@ -1,7 +1,5 @@
 package dk.dtu.model;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,33 +15,33 @@ public class ValidationTest {
 	}
 
 	@Test
-	public void testIsPositiveInteger() {
-		fail("Not yet implemented");
+	public void testIsPositiveInteger() throws ValidationException {
+		Validation.isPositiveInteger("7");
 	}
 
 	@Test
-	public void testIsOnlyLetters() {
-		fail("Not yet implemented");
+	public void testIsOnlyLetters() throws ValidationException {
+		Validation.isOnlyLetters("egrtjtyERHRT");
 	}
 
 	@Test
-	public void testAuthenticateUser() {
-		fail("Not yet implemented");
+	public void testAuthenticateUser() throws ValidationException {
+		Validation.authenticateUser("admin", "root");
 	}
 
 	@Test
-	public void testIsValidID() {
-		fail("Not yet implemented");
+	public void testIsValidID() throws ValidationException {
+		Validation.isValidID("6755");
 	}
 
 	@Test
-	public void testIsValidUserName() {
-		fail("Not yet implemented");
+	public void testIsValidUserName() throws ValidationException {
+		Validation.isValidUserName("Peter Madsen");
 	}
 
 	@Test
-	public void testIsValidInitials() {
-		fail("Not yet implemented");
+	public void testIsValidInitials() throws ValidationException {
+		Validation.isValidInitials("PM");
 	}
 
 	@Test
@@ -51,34 +49,34 @@ public class ValidationTest {
 		Validation.isValidCpr("1208871443");
 	}
 
-	@Test
+	@Test(expected = ValidationException.class)
 	public void testIsInvalidCprDay() throws ValidationException {
 		Validation.isValidCpr("3208871443");
 	}
-	
+
 	@Test(expected = ValidationException.class)
 	public void testIsInvalidCprMonth() throws ValidationException {
 		Validation.isValidCpr("1213871443");
 	}
-	
+
 	@Test(expected = ValidationException.class)
 	public void testIsInvalidCprFutureDate() throws ValidationException {
 		Validation.isValidCpr("2902401443");
 	}
-	
+
 	@Test(expected = ValidationException.class)
 	public void testIsInvalidCpr() throws ValidationException {
-		Validation.isValidCpr("1208871443");
-	}
-	
-	@Test
-	public void testIsValidPassword() {
-		fail("Not yet implemented");
+		Validation.isValidCpr("1208871442");
 	}
 
 	@Test
-	public void testIsValidRole() {
-		fail("Not yet implemented");
+	public void testIsValidPassword() throws ValidationException {
+		Validation.isValidPassword("dfgERET");
+	}
+
+	@Test
+	public void testIsValidRole() throws ValidationException {
+		Validation.isValidRole("foreman");
 	}
 
 }

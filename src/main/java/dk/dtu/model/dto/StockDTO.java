0cdffb4d8fc915e1536117723a7f948b2f069e16
Christian Niemann <spirem@gmail.com>
2017-06-08 21:36:16 +0200

    StockDTO

---
+++ src/main/java/dk/dtu/model/dto/StockDTO.java
@@ -0,0 +1,30 @@
+package dk.dtu.model.dto;
+
+public class StockDTO {
+
+	private String produceName;
+	private double currentStock;
+	
+	public StockDTO() {}
+
+	public StockDTO(String produceName, double currentStock) {
+		this.produceName = produceName;
+		this.currentStock = currentStock;
+	}
+
+	public String getProduceName() {
+		return produceName;
+	}
+
+	public void setProduceName(String produceName) {
+		this.produceName = produceName;
+	}
+
+	public double getCurrentStock() {
+		return currentStock;
+	}
+
+	public void setCurrentStock(double currentStock) {
+		this.currentStock = currentStock;
+	}
+}

1afef9fdcf4432c7f332c6e0ec0714050c7ebb5f
Christian Niemann <spirem@gmail.com>
2017-06-08 22:07:59 +0200

    ProduceDAO

--- src/main/java/dk/dtu/model/dao/MySQLProduceDAO.java
+++ src/main/java/dk/dtu/model/dao/MySQLProduceDAO.java
@@ -15,33 +15,46 @@ public class MySQLProduceDAO implements ProduceDAO {
 
 	@Override
 	public void createProduce(ProduceDTO produce) throws DALException {
-		if(Connector.doUpdate("CALL create_produce('" + produce.getProduceName() + "', '" + produce.getSupplier() + "');") == 0) 
+		if(Connector.getInstance().doUpdate("CALL create_produce('"
+				+ produce.getProduceName() + "','"
+				+ produce.getSupplier() + "');") == 0) 
 		{
 			throw new DALException("No rows affected!");
 		}
 	}
-	
+
 	@Override
-	public ProduceDTO readProduce(int raavareId) throws DALException {
-		ResultSet rs = Connector.doQuery("CALL read_produce('" + raavareId + "');");
-		try
-		{
-			if (!rs.first()) throw new DALException("Produce with id " + raavareId + " does not exist");
-			return new ProduceDTO(rs.getInt("produce_id"), rs.getString("produce_name"), rs.getString("supplier"));
-		} catch (SQLException e) { throw new DALException(e); }
+	public ProduceDTO readProduce(int produceId) throws DALException {
+		ResultSet rs = Connector.getInstance().doQuery("CALL read_produce('" + produceId + "');");
+		try {
+			if (!rs.first()) {
+				throw new DALException("Produce with id " + produceId + " does not exist");
+			}
+			return new ProduceDTO(
+					rs.getInt("produce_id"),
+					rs.getString("produce_name"),
+					rs.getString("supplier"));
+		} catch (SQLException e) {
+			throw new DALException(e);
+		} finally {
+			Connector.getInstance().closeResources();
+		}
 	}
-	
+
 	@Override
 	public void updateProduce(ProduceDTO produce) throws DALException {
-		if(Connector.doUpdate("CALL update_produce('" + produce.getProduceId() + "', '" + produce.getProduceName() + "', '" + produce.getSupplier() + "');") == 0) 
+		if(Connector.getInstance().doUpdate("CALL update_produce('"
+				+ produce.getProduceId() + "','"
+				+ produce.getProduceName() + "','"
+				+ produce.getSupplier() + "');") == 0) 
 		{
 			throw new DALException("No rows affected!");
 		}
 	}
-	
+
 	@Override
 	public void deleteProduce(int raavareId) throws DALException {
-		if(Connector.doUpdate("CALL delete_produce(" + raavareId + ";") == 0)
+		if(Connector.getInstance().doUpdate("CALL delete_produce(" + raavareId + ");") == 0)
 		{
 			throw new DALException("No rows affected");
 		}
@@ -50,7 +63,7 @@ public class MySQLProduceDAO implements ProduceDAO {
 	@Override
 	public List<ProduceDTO> getProduceList() throws DALException {
 		List<ProduceDTO> list = new ArrayList<ProduceDTO>();
-		ResultSet rs = Connector.doQuery("SELECT * FROM produce;");
+		ResultSet rs = Connector.getInstance().doQuery("SELECT * FROM produce;");
 		try
 		{
 			while (rs.next())
@@ -59,17 +72,20 @@ public class MySQLProduceDAO implements ProduceDAO {
 						rs.getInt("produce_id"),
 						rs.getString("produce_name"),
 						rs.getString("supplier")
-				));
+						));
 			}
+			return list;
+		} catch (SQLException e) {
+			throw new DALException(e);
+		} finally {
+			Connector.getInstance().closeResources();
 		}
-		catch (SQLException e) { throw new DALException(e); }
-		return list;
 	}
 
 	@Override
 	public List<ProduceOverviewDTO> getProduceOverview() throws DALException{
 		List<ProduceOverviewDTO> list = new ArrayList<ProduceOverviewDTO>();
-		ResultSet rs = Connector.doQuery("SELECT * FROM produce_overview;");
+		ResultSet rs = Connector.getInstance().doQuery("SELECT * FROM produce_overview;");
 		try
 		{
 			while (rs.next())
@@ -77,11 +93,15 @@ public class MySQLProduceDAO implements ProduceDAO {
 				list.add(new ProduceOverviewDTO(
 						rs.getInt("produce_id"),
 						rs.getString("produce_name"),
+						rs.getString("supplier"),
 						rs.getDouble("amount")
-				));
+						));
 			}
+			return list;
+		} catch (SQLException e) {
+			throw new DALException(e);
+		} finally {
+			Connector.getInstance().closeResources();
 		}
-		catch (SQLException e) { throw new DALException(e); }
-		return list;
 	}
 }

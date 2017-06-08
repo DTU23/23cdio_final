26314175d67bad32e0c9e4db7f74fe6cdd73a74b
Christian Niemann <spirem@gmail.com>
2017-06-09 00:03:43 +0200

    ProductBatchDAO

--- src/main/java/dk/dtu/model/dao/MySQLProductBatchDAO.java
+++ src/main/java/dk/dtu/model/dao/MySQLProductBatchDAO.java
@@ -8,6 +8,7 @@ import java.util.List;
 import dk.dtu.model.connector.Connector;
 import dk.dtu.model.dto.ProductBatchCompOverviewDTO;
 import dk.dtu.model.dto.ProductBatchDTO;
+import dk.dtu.model.dto.ProductBatchListDTO;
 import dk.dtu.model.interfaces.DALException;
 import dk.dtu.model.interfaces.ProductBatchDAO;
 
@@ -15,87 +16,113 @@ public class MySQLProductBatchDAO implements ProductBatchDAO {
 
 	@Override
 	public void createProductBatch(int recipe_id) throws DALException {
-		if(Connector.doUpdate("CALL create_product_batch('"+recipe_id+"');") == 0) {
+		if(Connector.getInstance().doUpdate("CALL create_product_batch('"+recipe_id+"');") == 0)
+		{
 			throw new DALException("No rows affected!");
 		}
 	}
-	
+
 	@Override
 	public ProductBatchDTO readProductBatch(int pbId) throws DALException {
-		ResultSet rs = Connector.doQuery("CALL read_product_batch('"+pbId+"');");
-		try 
-		{
-			if (!rs.first()) throw new DALException("Product batch with id " + pbId + " does not exist");
-			return new ProductBatchDTO(rs.getInt("pb_id"), rs.getInt("status"), rs.getInt("recipe_id"));
-		} catch (SQLException e) { throw new DALException(e); }
-	}
-	
-	@Override
-	public void updateProductBatchStatus(ProductBatchDTO productbatch) throws DALException {
-		if(productbatch.getStatus() == 0 || productbatch.getStatus() == 1 || productbatch.getStatus() == 2) {
-			if(Connector.doUpdate("CALL update_product_batch('"+productbatch.getPbId()+"', '"+productbatch.getStatus()+"');") == 0) {
-				throw new DALException("No rows affected!");
+		ResultSet rs = Connector.getInstance().doQuery("CALL read_product_batch('"+pbId+"');");
+		try {
+			if (!rs.first()) {
+				throw new DALException("Product batch with id " + pbId + " does not exist");
 			}
-		} else {
-			throw new DALException("Invalid status provided!");
+			return new ProductBatchDTO(
+					rs.getInt("pb_id"),
+					rs.getInt("recipe_id"),
+					rs.getInt("status"));
+		} catch (SQLException e) {
+			throw new DALException(e);
+		} finally {
+			Connector.getInstance().closeResources();
 		}
 	}
-	
+
+	@Override
+	public void updateProductBatch(ProductBatchDTO productbatch) throws DALException {
+		if(Connector.getInstance().doUpdate("CALL update_product_batch('"
+				+ productbatch.getPbId()+"','"
+				+ productbatch.getRecipeId()+"','"
+				+ productbatch.getStatus()+"');") == 0)
+		{
+			throw new DALException("No rows affected!");
+		}
+	}
+
 	@Override
 	public void deleteProductBatch(int pbId) throws DALException {
-		if(Connector.doUpdate("CALL delete_product_batch(" + pbId + ";") == 0)
+		if(Connector.getInstance().doUpdate("CALL delete_product_batch(" + pbId + ";") == 0)
 		{
 			throw new DALException("No rows affected");
 		}
 	}
 
 	@Override
-	public ProductBatchCompOverviewDTO getProductBatchListDetailsByPbId(int productBatchID) throws DALException {
-		ResultSet rs = Connector.doQuery("CALL get_product_batch_list_details_by_pb_id("+productBatchID+");");
-		try {
-			if(!rs.first()) throw new DALException("Product batch with id " + productBatchID + " does not exist");
-			else return new ProductBatchCompOverviewDTO(rs.getInt("pb_id"), 0, rs.getInt("recipe_id"), rs.getString("recipe_name"), rs.getInt("status"), null, 0, 0);
-		} catch (SQLException e) {	throw new DALException(e); }
-	}
-
-	@Override
-	public List<ProductBatchCompOverviewDTO> getProductBatchDetailsByPbId(int productBatchID) throws DALException {
-		List<ProductBatchCompOverviewDTO> list = new ArrayList<ProductBatchCompOverviewDTO>();
-		ResultSet rs = Connector.doQuery("CALL get_product_batch_details_by_pb_id("+productBatchID+");");
-		
-		try {
-			while (rs.next())
-			{
-				list.add(new ProductBatchCompOverviewDTO(rs.getInt("pb_id"), rs.getInt("rb_id"), rs.getInt("recipe_id"), rs.getString("recipe_name"), rs.getInt("status"), rs.getString("produce_name"), rs.getDouble("netto"), rs.getInt("opr_id")));
-			}
-		} catch (SQLException e) {	throw new DALException(e); }
-		
-		return list;
-	}
-	
-	@Override
-	public List<ProductBatchDTO> getProductBatchList() throws DALException {
-		List<ProductBatchDTO> list = new ArrayList<ProductBatchDTO>();
-		ResultSet rs = Connector.doQuery("SELECT * FROM productbatch;");
+	public List<ProductBatchListDTO> getProductBatchList() throws DALException {
+		List<ProductBatchListDTO> list = new ArrayList<ProductBatchListDTO>();
+		ResultSet rs = Connector.getInstance().doQuery("SELECT * FROM product_batch_list;");
 		try
 		{
 			while (rs.next()) 
 			{
-				list.add(new ProductBatchDTO(rs.getInt("pb_id"), rs.getInt("status"), rs.getInt("recipe_id")));
+				list.add(new ProductBatchListDTO(
+						rs.getInt("pb_id"),
+						rs.getInt("recipe_id"),
+						rs.getString("recipe_name"),
+						rs.getInt("status")));
 			}
+			return list;
+		} catch (SQLException e) {
+			throw new DALException(e);
+		} finally {
+			Connector.getInstance().closeResources();
 		}
-		catch (SQLException e) { throw new DALException(e); }
-		return list;
 	}
-	
+
 	@Override
-	public boolean exists(int pbId) throws DALException{
-		ResultSet rs = Connector.doQuery("SELECT * FROM productbatch WHERE pb_id = " + pbId + ";");
+	public ProductBatchListDTO getProductBatchListDetailsByPbId(int pbId) throws DALException {
+		ResultSet rs = Connector.getInstance().doQuery("CALL get_product_batch_list_details_by_pb_id("+pbId+");");
 		try {
-			return rs.first();
+			if(!rs.first()) {
+				throw new DALException("Product batch with id " + pbId + " does not exist");
+			}
+			return new ProductBatchListDTO(
+					rs.getInt("pb_id"),
+					rs.getInt("recipe_id"),
+					rs.getString("recipe_name"),
+					rs.getInt("status"));
+		} catch (SQLException e) {
+			throw new DALException(e);
+		} finally {
+			Connector.getInstance().closeResources();
 		}
-		catch (SQLException e) {
-			return false;
+	}
+
+	@Override
+	public List<ProductBatchCompOverviewDTO> getProductBatchDetailsByPbId(int pbId) throws DALException {
+		List<ProductBatchCompOverviewDTO> list = new ArrayList<ProductBatchCompOverviewDTO>();
+		ResultSet rs = Connector.getInstance().doQuery("CALL get_product_batch_details_by_pb_id("+pbId+");");
+
+		try {
+			while (rs.next())
+			{
+				list.add(new ProductBatchCompOverviewDTO(
+						rs.getInt("pb_id"),
+						rs.getInt("rb_id"),
+						rs.getInt("recipe_id"),
+						rs.getString("recipe_name"),
+						rs.getInt("status"),
+						rs.getString("produce_name"),
+						rs.getDouble("netto"),
+						rs.getInt("opr_id")));
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DALException(e);
+		} finally {
+			Connector.getInstance().closeResources();
 		}
 	}
 }

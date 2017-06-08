f6c18169806c825977fa4c3579714de5529d7834
Christian Niemann <spirem@gmail.com>
2017-06-08 21:50:39 +0200

    ProduceBatchDAO

--- src/main/java/dk/dtu/model/interfaces/ProduceBatchDAO.java
+++ src/main/java/dk/dtu/model/interfaces/ProduceBatchDAO.java
@@ -3,12 +3,14 @@ package dk.dtu.model.interfaces;
 import java.util.List;
 
 import dk.dtu.model.dto.ProduceBatchDTO;
+import dk.dtu.model.dto.StockDTO;
 
 public interface ProduceBatchDAO {
 	void createProduceBatch(int produce_id, double amount) throws DALException;
 	ProduceBatchDTO readProduceBatch(int rbId) throws DALException;
 	void updateProduceBatch(int produce_id, double amount) throws DALException;
 	void deleteProduceBatch(int rbId) throws DALException;
-	ProduceBatchDTO getProduceBatchWithProduceName(int rbId) throws DALException;
 	List<ProduceBatchDTO> getProduceBatchList() throws DALException;
+	ProduceBatchDTO getProduceBatchWithProduceName(int rbId) throws DALException;
+	List<StockDTO> getProduceInStock() throws DALException;
 }

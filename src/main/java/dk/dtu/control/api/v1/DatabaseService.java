5c33eb3118771acbb752d17ab4ca9360ac0c129d
Christian Niemann <spirem@gmail.com>
2017-06-08 21:53:16 +0200

    connector change impact

--- src/main/java/dk/dtu/control/api/v1/DatabaseService.java
+++ src/main/java/dk/dtu/control/api/v1/DatabaseService.java
@@ -1,11 +1,7 @@
 package dk.dtu.control.api.v1;
 
-import java.sql.SQLException;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-
-import dk.dtu.model.connector.Connector;
 
 @Path("/v1/database")
 public class DatabaseService {
@@ -13,10 +9,10 @@ public class DatabaseService {
 	@GET
 	@Path("/init")
 	public void initDatabase(){
-		try {
-			new Connector();
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			new Connector();
+//		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+//			e.printStackTrace();
+//		}
 	}
 }

bb7d50376b31bc41e995f59d256c36f58d69fd27
Frederik Værnegaard <frederik@nyka.dk>
2017-06-08 21:25:45 +0200

    product service impl.

--- src/main/java/dk/dtu/control/api/v1/ProductService.java
+++ src/main/java/dk/dtu/control/api/v1/ProductService.java
@@ -3,8 +3,10 @@ package dk.dtu.control.api.v1;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -23,24 +25,42 @@ import dk.dtu.model.interfaces.ProductBatchDAO;
 @Consumes(MediaType.APPLICATION_JSON)
 public class ProductService {
 
+	// This class implements all MySQLProductBatchDAO methods
 	private ProductBatchDAO dao = new MySQLProductBatchDAO();
 
+	@POST
+	@Secured( roles = { Role.Foreman })
+	public void createProductBatch(int recipeId) throws DALException {
+		dao.createProductBatch(recipeId);
+	}
+	
 	@GET
 	@Path("/{id}")
-	@Secured( roles = { Role.Pharmacist })
-	public ProductBatchDTO getProduct(@PathParam("id") int pbId) throws ValidationException, DALException {
-		return dao.getProductBatch(pbId);
+	@Secured( roles = { Role.Foreman })
+	public ProductBatchDTO getProductBatch(@PathParam("id") int pbId) throws ValidationException, DALException {
+		return dao.readProductBatch(pbId);
 	}
-
+	
+	@PUT
+	@Secured( roles = { Role.Foreman })
+	public void updateProductBatch(ProductBatchDTO productBatch) throws DALException {
+		dao.updateProductBatchStatus(productBatch);
+	}
+	
+	@DELETE
+	@Secured( roles = { Role.Foreman })
+	public void deleteProductBatch(int pbId) throws DALException {
+		dao.deleteProductBatch(pbId);
+	}
+	
 	@GET
 	@Secured( roles = { Role.Foreman })
 	public List<ProductBatchDTO> getProductList() throws DALException {
 		return dao.getProductBatchList();
 	}
+	
+	//TODO Methods not implemented here from DAO - but necessary? They're used for weighing process.
+	//getProductBatchListDetailsByPbId
+	//getProductBatchDetailsByPbId
 
-	@POST
-	@Secured( roles = { Role.Foreman })
-	public void createProduct(ProductBatchDTO pb) throws DALException {
-
-	}
 }

578edf316e05a5177a8910c6525e181b223cb2c2
Viktor Poulsen <viktormaigaard@gmail.com>
2017-06-09 14:52:14 +0200

    Secured database service

--- src/main/java/dk/dtu/control/api/v1/DatabaseService.java
+++ src/main/java/dk/dtu/control/api/v1/DatabaseService.java
@@ -3,10 +3,13 @@ package dk.dtu.control.api.v1;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import dk.dtu.control.api.Secured;
 import dk.dtu.model.connector.Connector;
 import dk.dtu.model.interfaces.DALException;
+import dk.dtu.control.api.Role;
 
 @Path("/v1/database")
+@Secured(roles = {Role.Pharmacist}, admin = true)
 public class DatabaseService {
 
 	@GET

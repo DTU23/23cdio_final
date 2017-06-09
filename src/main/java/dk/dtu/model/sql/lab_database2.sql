e6a573be841ef6ad4368d7972f0ee614c4cead57
Unknown <spirem@gmail.com>
2017-06-09 12:50:17 +0200

    sql

--- src/main/java/dk/dtu/model/sql/lab_database2.sql
+++ src/main/java/dk/dtu/model/sql/lab_database2.sql
@@ -17,25 +17,51 @@ CREATE TABLE operator(
    role ENUM('Operator', 'Foreman', 'Pharmacist', 'None') DEFAULT 'None'
 ) ENGINE=innoDB;
  
-CREATE TABLE produce(produce_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, produce_name TEXT, supplier TEXT) ENGINE=innoDB;
+CREATE TABLE produce(
+   produce_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+   produce_name TEXT,
+   supplier TEXT
+) ENGINE=innoDB;
 
-CREATE TABLE producebatch(rb_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, produce_id INT, amount REAL,
-   FOREIGN KEY (produce_id) REFERENCES produce(produce_id)) ENGINE=innoDB;
+CREATE TABLE producebatch(
+   rb_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+   produce_id INT,
+   amount REAL,
+   FOREIGN KEY (produce_id) REFERENCES produce(produce_id)
+) ENGINE=innoDB;
 
-CREATE TABLE recipe(recipe_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, recipe_name TEXT) ENGINE=innoDB;
+CREATE TABLE recipe(
+   recipe_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+   recipe_name TEXT
+) ENGINE=innoDB;
 
-CREATE TABLE recipecomponent(recipe_id INT, produce_id INT, nom_netto REAL, tolerance REAL,
+CREATE TABLE recipecomponent(
+   recipe_id INT,
+   produce_id INT,
+   nom_netto REAL,
+   tolerance REAL,
    PRIMARY KEY (recipe_id, produce_id),
    FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id),
-   FOREIGN KEY (produce_id) REFERENCES produce(produce_id)) ENGINE=innoDB;
+   FOREIGN KEY (produce_id) REFERENCES produce(produce_id)
+) ENGINE=innoDB;
 
-CREATE TABLE productbatch(pb_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, recipe_id INT, status INT,
-   FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id)) ENGINE=innoDB;
+CREATE TABLE productbatch(
+   pb_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+   recipe_id INT,
+   status INT,
+   FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id)
+) ENGINE=innoDB;
 
-CREATE TABLE productbatchcomponent(pb_id INT, rb_id INT, tara REAL, netto REAL, opr_id INT,
+CREATE TABLE productbatchcomponent(
+   pb_id INT,
+   rb_id INT,
+   tara REAL,
+   netto REAL,
+   opr_id INT,
    PRIMARY KEY (pb_id, rb_id),
    FOREIGN KEY (pb_id) REFERENCES productbatch(pb_id),
    FOREIGN KEY (rb_id) REFERENCES producebatch(rb_id),
-   FOREIGN KEY (opr_id) REFERENCES operator(opr_id)) ENGINE=innoDB;
+   FOREIGN KEY (opr_id) REFERENCES operator(opr_id)
+) ENGINE=innoDB;
 
 CALL reset_data();

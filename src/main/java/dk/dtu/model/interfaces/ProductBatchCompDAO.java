5592c7b7ce68ac9532f71e8f0e112f7b958f73d0
Christian Niemann <spirem@gmail.com>
2017-06-12 22:18:55 +0200

    interfaces

--- src/main/java/dk/dtu/model/interfaces/ProductBatchCompDAO.java
+++ src/main/java/dk/dtu/model/interfaces/ProductBatchCompDAO.java
@@ -5,14 +5,16 @@ import java.util.List;
 import dk.dtu.model.dto.ProductBatchCompDTO;
 import dk.dtu.model.dto.ProductBatchCompOverviewDTO;
 import dk.dtu.model.dto.ProductBatchCompSupplierDetailsDTO;
-import dk.dtu.model.exceptions.DALException;
+import dk.dtu.model.exceptions.dal.ConnectivityException;
+import dk.dtu.model.exceptions.dal.IntegrityConstraintViolationException;
+import dk.dtu.model.exceptions.dal.NotFoundException;
 
 public interface ProductBatchCompDAO {
-	void createProductBatchComp(ProductBatchCompDTO productbatchcomponent) throws DALException;
-	ProductBatchCompDTO readProductBatchComp(int pbId, int rbId) throws DALException;
-	void updateProductBatchComp(ProductBatchCompDTO productBatchComponent) throws DALException;
-	void deleteProductBatchComp(int pbId, int rbId) throws DALException;
-	List<ProductBatchCompDTO> getProductBatchCompList() throws DALException;
-	List<ProductBatchCompOverviewDTO> getProductBatchCompOverview() throws DALException;
-	List<ProductBatchCompSupplierDetailsDTO> getProductBatchComponentSupplierDetailsByPbId(int pbId) throws DALException;
+	void createProductBatchComp(ProductBatchCompDTO productbatchcomponent) throws ConnectivityException, IntegrityConstraintViolationException;
+	ProductBatchCompDTO readProductBatchComp(int pbId, int rbId) throws ConnectivityException, NotFoundException;
+	void updateProductBatchComp(ProductBatchCompDTO productBatchComponent) throws ConnectivityException, NotFoundException, IntegrityConstraintViolationException;
+	void deleteProductBatchComp(int pbId, int rbId) throws ConnectivityException, NotFoundException, IntegrityConstraintViolationException;
+	List<ProductBatchCompDTO> getProductBatchCompList() throws ConnectivityException, NotFoundException;
+	List<ProductBatchCompOverviewDTO> getProductBatchCompOverview() throws ConnectivityException, NotFoundException;
+	List<ProductBatchCompSupplierDetailsDTO> getProductBatchComponentSupplierDetailsByPbId(int pbId) throws ConnectivityException, NotFoundException;
 }

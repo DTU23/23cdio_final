1ddde526d981e3eb7d7606c55339524d40f35f5b
Christian Niemann <spirem@gmail.com>
2017-06-15 14:15:18 +0200

    SQL
    fixed so dummy data can pass password validation

--- src/main/java/dk/dtu/model/sql/procedures/misc.sql
+++ src/main/java/dk/dtu/model/sql/procedures/misc.sql
@@ -14,8 +14,8 @@ CREATE PROCEDURE reset_data()
     DELETE FROM producebatch;
     DELETE FROM produce;
     INSERT INTO operator(opr_id, opr_name, ini, cpr, password, admin, role) VALUES
-      (1, 'Angelo A', 'AA', '0707707003', 'lKje4fa', FALSE, 'Foreman'),
-      (2, 'Antonella B', 'AB', '0808802236', 'atoJ21v', FALSE, 'Pharmacist'),
+      (1, 'Angelo A', 'AA', '0707707003', 'lKjE4fa', FALSE, 'Foreman'),
+      (2, 'Antonella B', 'AB', '0808802236', 'aToJ21v', FALSE, 'Pharmacist'),
       (3, 'Luigi C', 'LC', '0909909007', 'jEfm5aQ', FALSE, 'Operator'),
       (4, 'Super User', 'SU', '0109162407', 'root', TRUE, 'Pharmacist'),
       (5, 'Admin', 'ADM', '0109162407', 'root', TRUE, 'None'),

2e939e13dda9d0e75df696268cb978de1f96ef3e
Christian Niemann <spirem@gmail.com>
2017-06-16 15:40:49 +0200

    DatabaseService fast fill data

--- src/main/java/dk/dtu/control/api/v1/DatabaseService.java
+++ src/main/java/dk/dtu/control/api/v1/DatabaseService.java
@@ -21,4 +21,57 @@ public class DatabaseService {
 		return "Database reset successfully!";
 	}
 	
+	@GET
+	@Path("/users")
+	public String insertUsers() throws DALException {
+		if (DataSource.getInstance().insertUsers() == 0) {
+			throw new DALException("No rows affected!");
+		}
+		return "Users inserted successfully!";
+	}
+	
+	@GET
+	@Path("/produce")
+	public String insertProduce() throws DALException {
+		if (DataSource.getInstance().insertProduce() == 0) {
+			throw new DALException("No rows affected!");
+		}
+		return "Produce inserted successfully!";
+	}
+	
+	@GET
+	@Path("/recipe")
+	public String insertRecipe() throws DALException {
+		if (DataSource.getInstance().insertRecipe() == 0) {
+			throw new DALException("No rows affected!");
+		}
+		return "Recipe inserted successfully!";
+	}
+	
+	@GET
+	@Path("/producebatch")
+	public String insertProduceBatch() throws DALException {
+		if (DataSource.getInstance().insertProduceBatch() == 0) {
+			throw new DALException("No rows affected!");
+		}
+		return "Produce batch inserted successfully!";
+	}
+	
+	@GET
+	@Path("/productbatch")
+	public String insertProductBatch() throws DALException {
+		if (DataSource.getInstance().insertProductBatch() == 0) {
+			throw new DALException("No rows affected!");
+		}
+		return "Product batch inserted successfully!";
+	}
+	
+	@GET
+	@Path("/productbatchcomp")
+	public String insertProductBatchComp() throws DALException {
+		if (DataSource.getInstance().insertProductBatchComp() == 0) {
+			throw new DALException("No rows affected!");
+		}
+		return "Product batch components inserted successfully!";
+	}
 }
